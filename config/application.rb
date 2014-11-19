7720e7ef87faf0fadc985eb93ed1843c23a74dc7
Seth Reece <sethorama@bassplayer.st>
2014-11-18 18:03:18 -0800

    Added test for user first name, last name, and user name correctness

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Treebook
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+    I18n.enforce_available_locales = false
   end
 end
