b9f95c4414246adb69520fdf243f57775fc1988d
Seth Reece <sethorama@bassplayer.st>
2014-11-19 09:08:07 -0800

    created simple url paths

---
+++ app/controllers/sessions/sessions_controller.rb
@@ -0,0 +1,20 @@
+class Sessions::SessionsController < Devise::SessionsController
+ before_filter :configure_sign_in_params, only: [:create]
+
+   
+   def new
+    super
+   end
+
+ 
+  def create
+    super
+  end
+
+   
+  private
+
+  def configure_sign_in_params 
+    params.require(:user).permit(:email, :password) 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Treebook::Application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations', sessions: 'sessions/sessions' }
+
+  devise_scope :user do 
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
   devise_for :views
   resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
   root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
