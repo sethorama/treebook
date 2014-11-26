458ff310a480206b8958465359de72a323a8db1d
Seth Reece <sethorama@bassplayer.st>
2014-11-26 13:54:56 -0800

    also removed acceptance email for heroku

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -6,7 +6,8 @@ class UserFriendship < ActiveRecord::Base
 	after_destroy :delete_mutual_friendship!
 
 	state_machine :state, initial: :pending do
-		after_transition on: :accept, do: [:send_acceptance_email, :accept_mutual_friendship!]
+		# add :send_accptance_email to after_transition on: :accept when email service in place
+		after_transition on: :accept, do: [:accept_mutual_friendship!]
 		after_transition on: :block, do: [:block_mutual_friendship!]
 
 
