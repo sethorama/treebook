fbcbea8544e1fe472b2444c19af6669b15a13d26
Seth Reece <sethorama@bassplayer.st>
2014-11-27 10:54:12 -0800

    added file uploading and displaying on status update

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -16,6 +16,7 @@ class StatusesController < ApplicationController
   # GET /statuses/new
   def new
     @status = Status.new
+    @status.build_document
   end
 
   # GET /statuses/1/edit
@@ -75,6 +76,6 @@ class StatusesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def status_params
-      params.require(:status).permit(:name, :user_id, :content) if params[:status]
+      params.require(:status).permit(:content, :profile_name, :full_name, :user_id, :first_name, :last_name, document_attributes: [:attachment, :document, :attachment_file_name, :document_fields, :build_document, :remove_attachment, :user_id]) if params[:status]
     end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,4 @@
 class Document < ActiveRecord::Base
-	has_attached_file :attachment
+	has_attached_file :attachment, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
 end
