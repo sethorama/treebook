115dcc113a9e39a44ca0865c900b5c1a1dfe0439
Seth Reece <sethorama@bassplayer.st>
2014-11-28 14:28:30 -0800

    added resizing of images/thumbs

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,8 +41,20 @@ class User < ActiveRecord::Base
 
   has_many :accepted_friends, through: :accepted_user_friendships, source: :friend
 
-  has_attached_file :avatar
+  has_attached_file :avatar, styles: {
+    large: "800x800>", medium: "300x200>", small: "260x180>", thumb: "80x80#"
+  }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  def self.get_gravatars
+    all.each do |user|
+      if !user.avatar?
+        user.avatar = URI.parse(user.gravatar_url)
+        user.save
+        print "."
+      end
+    end
+  end
 
   def full_name
   	first_name + " " + last_name
