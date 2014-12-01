aa0f8c0c15561ff5aa9ced953ffdce5075213240
Seth Reece <sethorama@bassplayer.st>
2014-12-01 14:40:41 -0800

    added push updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  private
+  def render_permission_error
+  	render file: 'public/permission_error', status: :error, layout: false
+  end
+
+  def render_404
+		render file: 'public/404', status: :not_found
+  end
 end
